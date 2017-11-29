6f2b4081a7d2b60b127b3e044971c50d5bf6d318
yoon <xotlr92@gmail.com>
2017-11-29 12:32:18 +0900

    기본 셋팅

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+var port = 3000;
+
+app.get('/', function(req,res){
+    res.send('first app');
+});
+
+app.listen(port, function(){
+    console.log('connected', port);
+});
