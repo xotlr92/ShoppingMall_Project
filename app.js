9873247705a14c78be743fe8a0b90db4a2a41f79
yoon <xotlr92@gmail.com>
2017-11-30 09:33:07 +0900

    기본 셋팅(라우터설정)

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 
+var admin = require('./routes/admin');
+
 var app = express();
 var port = 3000;
+
+app.use('/admin', admin);
 
 app.get('/', function(req,res){
     res.send('first app');
