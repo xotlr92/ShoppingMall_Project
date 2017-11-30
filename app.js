a2d274b1c1478f53f2726729576775ed1079a8b2
yoon <xotlr92@gmail.com>
2017-11-30 11:28:51 +0900

    제품 등록 페이지 작성

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser'); // form에서 넘어온 데이터를 javascript 객체로 맵핑
+var logger = require('morgan'); // post, get 요청시 console에 로깅
 
 // mongoose 설정
 var mongoose = require('mongoose');
@@ -22,6 +24,11 @@ var port = 3000;
 // path.join : 현재 디렉토리 위치 + views 경로
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// 미들웨어 설정 - 항상 라우팅 전에
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 // 라우팅
 app.use('/admin', admin);
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
+// model 모듈 불러오기
+var ProductsModel = require('../models/ProductsModel');
 
 router.get('/', function(req,res){
     res.send('admin app');
 });
 router.get('/products', function(req,res){
     res.render('admin/products', {message:"hello"});
+});
+// 제품 등록 페이지 작성
+router.get('/products/write', function(req,res){
+    res.render('admin/form');
+});
+router.post('/products/write', function(req,res){
+    var product = new ProductsModel({
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description
+    });
+    product.save(function(err){
+        res.redirect('/admin/products'); // 성공시 redirect
+    });
 });
 
 module.exports = router;
