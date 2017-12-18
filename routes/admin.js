18a090eeab1c2cf8ac03e1a9c9fff2ec683abd36
yoon <xotlr92@gmail.com>
2017-12-18 11:30:24 +0900

    google chart 연동

--- routes/admin.js
+++ routes/admin.js
@@ -130,4 +130,28 @@ router.get('/order/edit/:id', adminRequired, function(req,res){
     });
 });
 
+router.get('/statistics', adminRequired, function(req,res){
+    CheckoutModel.find(function(err, orderList){
+        var barData = [];
+        var pieData = [];
+        orderList.forEach(function(order){
+            var date = new Date(order.created_at);
+            var monthDay = (date.getMonth()+1) + '-' + date.getDate();
+
+            if(monthDay in barData){
+                barData[monthDay]++;
+            } else {
+                barData[monthDay] = 1;
+            }
+
+            if(order.status in pieData){
+                pieData[order.status]++;
+            } else {
+                pieData[order.status] = 1;
+            }
+        });
+        res.render('admin/statistics', {barData:barData, pieData:pieData});
+    });
+});
+
 module.exports = router;
